a85f0c9b3c79ef5b67d8577e67661fbb5b3c845f
Tunji Abioye <danielabioye17@gmail.com>
2018-12-25 20:52:27 +0100

    Update route.py

--- main_app/routes.py
+++ main_app/routes.py
@@ -23,7 +23,7 @@ def login():
             next_page = request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('Requests'))
         else:
-            flash('You had previously unsucceful login attempts', 'danger')
+            flash('You have previously unsuccessful login attempts', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
